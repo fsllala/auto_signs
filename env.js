f9e5987373a89cac886621ecdc492d24cb698aec
fsllala <44030722+fsllala@users.noreply.github.com>
2023-10-30 19:40:12 +0800

    修改env

--- env.js
+++ env.js
@@ -1,14 +1,13 @@
-
-const PUSH_URL = "http://www.pushplus.plus/send"; // pushplus 
+const PUSH_URL = "http://www.pushplus.plus/send"; // pushplus
 const PUSH_PLUS_TOKEN = process.env.PUSH_PLUS_TOKEN;
 
 // ikun
-const USERNAME=process.env.USERNAME;
-const PASSWORD=process.env.PASSWORD;
+const USERNAME = process.env.USER;
+const PASSWORD = process.env.PWD;
 
-module.exports={
-    PUSH_URL,
-    PUSH_PLUS_TOKEN,
-    USERNAME,
-    PASSWORD
-}
+module.exports = {
+  PUSH_URL,
+  PUSH_PLUS_TOKEN,
+  USERNAME,
+  PASSWORD,
+};
