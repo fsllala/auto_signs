e6c59e5442352543dba51a6df8595ecab1efd26f
fsllala <44030722+fsllala@users.noreply.github.com>
2024-02-26 10:33:44 +0800

    Update ikunReq.js

--- plat/ikun/ikunReq.js
+++ plat/ikun/ikunReq.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 const ikuunInstance = axios.create({
-  baseURL: "https://ikuuu.me",
+  baseURL: "https://ikuuu.pw",
   headers: {
     accept: "application/json, text/javascript, */*; q=0.01",
     "accept-language": "zh-CN,zh;q=0.9",
@@ -14,7 +14,7 @@ const ikuunInstance = axios.create({
     "sec-fetch-mode": "cors",
     "sec-fetch-site": "same-origin",
     "x-requested-with": "XMLHttpRequest",
-    Referer: "https://ikuuu.me/user",
+    Referer: "https://ikuuu.pw/user",
     "Referrer-Policy": "strict-origin-when-cross-origin",
   },
 });
